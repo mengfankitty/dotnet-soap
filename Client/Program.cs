df2274b34fcedbb5c293648e7ab3c2986ff21bc2
walle <walle@evy.null>
2019-02-11 19:43:15 +0800

    add proxy02

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ServiceModel;
 using Client.Proxy01;
+using Client.Proxy02;
 
 namespace Client
 {
@@ -13,14 +14,36 @@ namespace Client
             var x = (int) (numGen.NextDouble() * 100);
             var y = (int) (numGen.NextDouble() * 100);
 
-            const string serviceAddress = "http://localhost:5000/calculator.svc";
+            const string endpointAddress = "http://localhost:5000/calculator.svc";
 
-            var client = new CalculatorClient(new BasicHttpBinding(), new EndpointAddress(serviceAddress));
+            RunProxy01(endpointAddress, x, y);
+            RunProxy02(endpointAddress, x, y);
+        }
 
-            Console.WriteLine($"{x} + {y} == {client.Add(x, y)}");
-            Console.WriteLine($"{x} - {y} == {client.Subtract(x, y)}");
-            Console.WriteLine($"{x} * {y} == {client.Multiply(x, y)}");
-            Console.WriteLine($"{x} / {y} == {client.Divide(x, y)}");
+        static void RunProxy01(string endpointAddress, int x, int y)
+        {
+            Console.WriteLine("Run Proxy01 {");
+
+            var client = new CalculatorClient(new BasicHttpBinding(), new EndpointAddress(endpointAddress));
+
+            Console.WriteLine($"  {x} + {y} == {client.Add(x, y)}");
+            Console.WriteLine($"  {x} - {y} == {client.Subtract(x, y)}");
+            Console.WriteLine($"  {x} * {y} == {client.Multiply(x, y)}");
+            Console.WriteLine($"  {x} / {y} == {client.Divide(x, y)}");
+            Console.WriteLine("}");
+        }
+
+        static void RunProxy02(string endpointAddress, int x, int y)
+        {
+            Console.WriteLine("Run Proxy02 {");
+
+            var client = new CalculatorServiceClient(CalculatorServiceClient.EndpointConfiguration.BasicHttpBinding_ICalculatorService, endpointAddress);
+
+            Console.WriteLine($"  {x} + {y} == {client.AddAsync(x, y).Result}");
+            Console.WriteLine($"  {x} - {y} == {client.SubtractAsync(x, y).Result}");
+            Console.WriteLine($"  {x} * {y} == {client.MultiplyAsync(x, y).Result}");
+            Console.WriteLine($"  {x} / {y} == {client.DivideAsync(x, y).Result}");
+            Console.WriteLine("}");
         }
     }
 }
